d17ecb7bb2d528378ed24a6849ead56ffcc35ea7
Surjan Mukherjee <111224611+SurjanMukherjee@users.noreply.github.com>
2025-08-20 17:44:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,18 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "eu-west-2" # ✅ Change if you have a different assigned region
+  default     = "eu-west-2" 
 }
 
 variable "instance_type" {
   description = "Instance type for EC2"
   type        = string
-  default     = "t2.micro"   # ✅ Fixed as per requirement
+  default     = "t2.micro"   
 }
 
 variable "key_name" {
   description = "Name of the existing key pair to use for EC2"
   type        = string
-  default     = "surjan1" # ✅ Replace with your actual key pair name
+  default     = "surjan1"
 }
+
