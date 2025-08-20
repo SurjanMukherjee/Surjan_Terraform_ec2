9738a5d4ac6de656af917b371ccccbe31234cd6f
Surjan Mukherjee <111224611+SurjanMukherjee@users.noreply.github.com>
2025-08-20 17:42:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "eu-west-2"
+}
+
 provider "aws" {
-  region     = "eu-west-2"
-  access_key = "AKIA5HTNIMZUCH5XMLJL"
-  secret_key = "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"
+  region = var.aws_region
 }
