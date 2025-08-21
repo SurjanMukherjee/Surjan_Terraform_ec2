b241b60eb28a1913fde85f60738a1511730dd575
Surjan Mukherjee <111224611+SurjanMukherjee@users.noreply.github.com>
2025-08-21 10:15:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,22 @@
-variable "aws_region" {
-  description = "AWS region to deploy to"
-  type        = string
-  default     = "eu-west-2"
+terraform {
+  #   backend "s3" {
+  #     bucket       = "surjan-terraform-s3-20082024"
+  #     key          = "surjan-terraform.tfstate"
+  #     region       = "eu-west-2"
+  #     use_lockfile = true
+  #   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
 
+# provider "aws" {
+#   region = "eu-west-2"
+# }
+
 provider "aws" {
-  region = var.aws_region
+  region = "eu-west-2" # Change to your assigned region
 }
